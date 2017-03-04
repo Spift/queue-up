0fe0e97ffeac4f969d5c255ee3bd52852fc85bdf
Simon Eriksen <simon.ramlochun@gmail.com>
2017-03-04 13:17:02 +0100

    The room preview in joinRoom view now automatically searches for data as you type which feels more intuitive and saves us having to use a button

--- Teachers_Pet/www/js/Controllers/joinRoomController.js
+++ Teachers_Pet/www/js/Controllers/joinRoomController.js
@@ -5,14 +5,78 @@
         .module('joinRoomController', [])
         .controller('joinRoomController', joinRoomController);
 
-    function joinRoomController($scope, $stateParams, $state, $firebaseObject, $firebaseArray, firebaseDataService, $ionicLoading, $ionicNavBarDelegate) {
+    function joinRoomController($scope, $stateParams, $state, $firebaseObject, $firebaseArray, firebaseDataService, $ionicLoading, $ionicNavBarDelegate, roomDataService) {
     	console.log('entry page controller fired');
-        $scope.validRoomData = false; // show/hide room info box
+        $scope.validRoomData = false; // show/hide room info preview
+        $scope.showLoadingSpinner = false; // show/hide the loading thing
+        $scope.showRoomNotFoundError = false;
         $scope.formData = {};
+        var roomCodeLength = 5; // the standard length of a room Code: if innput field text has this length, we automatically look in firebase for a room. no need for a button press
         /*
-         * submit pressed. The promise resolution should be moved to the service which should return the data in a firebaseObject. Will do later
+         * Every time the roomCode input field changes, if it has the right length, search firebase for a valid room
          */
-        $scope.submitPressed = function(roomCode) {
+        $scope.$watch('formData', function() {
+            if($scope.formData.code != undefined && $scope.formData.code.length == roomCodeLength) {
+                findRoomInDatabase($scope.formData.code);
+            }else{
+                $scope.validRoomData = false;
+                $scope.showRoomNotFoundError = false;
+            }
+        }, true);
+        /*
+         * Join Room button was pressed, so goto student help queue. Or admin queue if admin code was entered
+         */
+        $scope.joinRoomPressed = function(formToClear) {
+            $scope.validRoomData = false;
+            $scope.showRoomNotFoundError = false;
+            $scope.showLoadingSpinner = false;
+            $scope.formData = {};
+            $state.go("studentHelpQueue");
+        };
+        /*
+         * Close room info preview and reset input field 
+         */
+        $scope.closePreview = function(formToClear) {
+            console.log("close preview pressed");
+            $scope.validRoomData = false;
+            $scope.formData = {};
+            formToClear.$setPristine();
+            formToClear.$setUntouched();
+        };
+        /*
+         * Look up room in firebaseDB
+         */
+        function findRoomInDatabase(roomCode) {
+            $scope.showLoadingSpinner = true;
+            $scope.room = firebaseDataService.getRoom(roomCode);
+            $scope.room.$loaded()
+            .then(function() {
+                //data is now available!
+                $scope.showLoadingSpinner = false;
+                if($scope.room.$value !== null) { // make sure a room with given code actually exists.
+                    roomDataService.setRoom($scope.room); // store the room data in  roomDataService, so it can be accessed later by the queue view.
+                    $scope.validRoomData = true; // will make room preview appear
+                }else{
+                    $scope.showRoomNotFoundError = true;
+                    console.log("no such room :(");
+                }
+            })
+            .catch(function (err) {
+                // This is where errors land. We should show an UI error as well here
+                console.log('joinRoomController.findRoomInDatabase() Error', err.code);
+            });
+        }
+
+        // Update the title of the view
+        $ionicNavBarDelegate.title('Join Room!');
+        //Show back button
+        $ionicNavBarDelegate.showBackButton(true);
+    }
+})();
+
+
+/*
+THE OLD submitpressed function in case ill need it ....
             showLoadingDialog();
             //data promise resolution
             firebaseDataService.getRoom(roomCode)
@@ -32,50 +96,4 @@
                 hideLoadingDialog();
                 console.log('joinRoomController.submitPressed() Error', err.code);
             });
-        }
-        /*
-         * Join Room button was pressed, so goto student help queue. Or admin queue if admin code was entered
-         */
-        $scope.joinRoomPressed = function(formToClear) {
-            $scope.validRoomData = false;
-            $scope.formData = {};
-            $state.go("studentHelpQueue");
-        };
-        /*
-         * Close room info card and reset input field 
-         */
-        $scope.closeCard = function(formToClear) {
-            console.log("close card pressed");
-            $scope.validRoomData = false;
-            $scope.formData = {};
-            formToClear.$setPristine();
-            formToClear.$setUntouched();
-        };
-        /*
-         * show the loading dialog
-         */
-        function showLoadingDialog() {
-            $ionicLoading.show({
-              template: '<p>Fetching Stuff...</p><ion-spinner></ion-spinner>',
-            })
-            .then(function(){
-               console.log("The loading indicator is now displayed");
-            });
-        };
-        /*
-         * show the loading dialog
-         */
-        function hideLoadingDialog(){
-            $ionicLoading.hide().then(function(){
-               console.log("The loading indicator is now hidden");
-            });
-        }
-
-        // Update the title of the view
-        $ionicNavBarDelegate.title('Join Room!');
-        //Show back button
-        $ionicNavBarDelegate.showBackButton(true);
-    }
-
-
-})();
+            */
--- Teachers_Pet/www/js/Services/firebaseDataService.js
+++ Teachers_Pet/www/js/Services/firebaseDataService.js
@@ -8,44 +8,38 @@
     /*
      * Fetch (mainly room?) Data from Firebase DB.
      * TODO: make a function in which the data is returned as a firebaseObject so we can have the live update feature which is needed for the question list
+     * ***********************
+
+
+     just return firebaseObject(ref) in resolution...
+     
+
      */
     function firebaseDataService($firebaseObject, $firebaseArray, $ionicLoading) {
         /*
          * Fetch a room listed in the Rooms database.
          */
         function getRoom(roomCode) {
-            var ref = firebase.database().ref("Rooms/" + roomCode);
-            //var people = $firebaseArray(peopleRef);
-            var promise = ref.once('value');
-            return promise;
-
             //TODO: add some logic for distinguishing between room codes and ADMIN codes.
             //Maybe if we have the length of the two codes be different
             //that would be a good way of easily knowing one from the other...
             //var roomRef = firebase.database().ref("Rooms/" + roomCode);
             //var room = $firebaseArray(roomRef);
             //return room;
+            var ref = firebase.database().ref("Rooms/" + roomCode);
+            var room = $firebaseObject(ref);
+                    
+            return room;
         }
         /*
-         * show the loading dialog
+         * Fetch a room listed in the Rooms database. THIS IS NOT USED:.....:::::
          */
-        function showLoadingDialog() {
-            $ionicLoading.show({
-              template: '<p>Fetching Stuff...</p><ion-spinner></ion-spinner>'
-            })
-            .then(function(){
-               console.log("The loading indicator is now displayed");
-            });
-        };
-        /*
-         * hide the loading dialog
-         */
-        function hideLoadingDialog(){
-            $ionicLoading.hide().then(function(){
-               console.log("The loading indicator is now hidden");
-            });
-        };
-
+        function getRoomOLD(roomCode) {
+            var ref = firebase.database().ref("Rooms/" + roomCode);
+            //var people = $firebaseArray(peopleRef);
+            var promise = ref.once('value');
+            return promise;
+        }
         /*
          * Available methods that the service should offer to the controller must be listed here:
          */
