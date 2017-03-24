1d8489c5a19d314ca0d94725f52a86b6cf32ec64
Emil Slot Arakelian Jensen <sbh280@alumni.ku.dk>
2017-03-24 14:31:55 +0100

    Fixed som help buttons

--- Teachers_Pet/www/js/Controllers/studentHelpQueueController.js
+++ Teachers_Pet/www/js/Controllers/studentHelpQueueController.js
@@ -11,42 +11,28 @@
         $scope.studentID = localStorageService.getStudentID();
         $scope.room = roomDataService.getRoom();
         $scope.Qs = roomDataService.getQuestions();
-        $scope.visibleQuestion = 0; // TODO: make so YOUR question is the one that is expanded upon first opening the view...
-        $scope.Qs = firebaseDataService.getQuestions($scope.room.$id);
-        $scope.ownQuestionVisible = true;
-        $scope.ownQuestionIndex = -1;
+        $scope.visibleQuestion = -1;
+        // $scope.ownQuestionVisible = true;
+        // $scope.ownQuestionIndex = -1;
 
-        setTimeout(function() {
-            var i = 0;
-            for(var quest in $scope.Qs) {
-                var key = $scope.Qs.$keyAt(i);
-                var q = $scope.Qs.$getRecord(key);
-                if (q.studentID == $scope.studentID){
-                    $scope.visibleQuestion = i;
-                    break;
-                }
-                i++;
+        var i = 0;
+        for(var i = 0; i < $scope.Qs.length; i++) {
+            var key = $scope.Qs.$keyAt(i);
+            var q = $scope.Qs.$getRecord(key);
+            console.log("Question id: " + q.studentID);
+            console.log("Scope id: " + $scope.studentID);
+            if (q.studentID == $scope.studentID){
+                $scope.visibleQuestion = i;
+                break;
             }
-        }, 0);
+        }
 
         /*
          * When a header is clicked, toggle the visibility of the body
          */
         $scope.expandQuestion = function(index) {
-                    var i = 0;
-            for(var quest in $scope.Qs) {
-                var key = $scope.Qs.$keyAt(i);
-                var q = $scope.Qs.$getRecord(key);
-                if (q.studentID == $scope.studentID){
-                    $scope.visibleQuestion = i;
-                }
-                i++;
-            }
             if($scope.visibleQuestion == index) {
                 $scope.visibleQuestion = -1;//all questions are collapsed now
-                // if(index == $scope.ownQuestionIndex){
-                //     $scope.ownQuestionVisible = false;
-                // }
             }else{
                 $scope.visibleQuestion = index;
             }
